17d3e65fa3f056b1152908339a4d55ce0608cff0
kayanalqusur-ai <kayanalqusur@gmail.com>
2025-09-28 23:28:38 +0300

    markode

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes.js";
 import { setupVite, serveStatic, log } from "./vite.js";
 import "dotenv/config";
+import 'module-alias/register.js';
 
 const app = express();
 app.use(express.json());

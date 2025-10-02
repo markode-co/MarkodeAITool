f09de281ff6a2a014680369193a7baecd5a2bc2c
kayanalqusur-ai <kayanalqusur@gmail.com>
2025-10-03 02:01:32 +0300

    Markode

--- build-copy.js
+++ build-copy.js
@@ -3,7 +3,6 @@ import fs from "fs";
 import path from "path";
 
 const src = path.join("dist", "public");
-
 const dest = path.join("dist", "server", "public");
 
 fs.mkdirSync(dest, { recursive: true });
@@ -13,7 +12,6 @@ function copyRecursive(srcDir, destDir) {
   for (const entry of entries) {
     const srcPath = path.join(srcDir, entry.name);
     const destPath = path.join(destDir, entry.name);
-
     if (entry.isDirectory()) {
       fs.mkdirSync(destPath, { recursive: true });
       copyRecursive(srcPath, destPath);
